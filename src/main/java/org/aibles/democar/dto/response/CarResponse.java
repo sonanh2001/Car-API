3527a207b161514be23d5d4f26a4220143a885a4
sonanh2001 <anhnhs.b19cn026@stu.ptit.edu.vn>
2022-08-26 16:15:20 +0700

    fix : remove extend relationship between CarResponse and CreateCarRequest, add getter and setter for CarResponse

--- src/main/java/org/aibles/democar/dto/response/CarResponse.java
+++ src/main/java/org/aibles/democar/dto/response/CarResponse.java
@@ -1,14 +1,22 @@
 package org.aibles.democar.dto.response;
 
-import org.aibles.democar.dto.request.CreateCarRequest;
 import org.aibles.democar.entity.Car;
 
-public class CarResponse extends CreateCarRequest {
+public class CarResponse {
   private long carId;
+  private String name;
 
-  public CarResponse() {
-    super();
-  }
+  private String brand;
+
+  private int wheelsNumber;
+
+  private String color;
+
+  private long price;
+
+  private int publicationDate;
+
+  public CarResponse() {}
 
   public long getCarId() {
     return carId;
@@ -16,6 +24,54 @@ public class CarResponse extends CreateCarRequest {
 
   public void setCarId(long carId) {
     this.carId = carId;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getBrand() {
+    return brand;
+  }
+
+  public void setBrand(String brand) {
+    this.brand = brand;
+  }
+
+  public int getWheelsNumber() {
+    return wheelsNumber;
+  }
+
+  public void setWheelsNumber(int wheelsNumber) {
+    this.wheelsNumber = wheelsNumber;
+  }
+
+  public String getColor() {
+    return color;
+  }
+
+  public void setColor(String color) {
+    this.color = color;
+  }
+
+  public long getPrice() {
+    return price;
+  }
+
+  public void setPrice(long price) {
+    this.price = price;
+  }
+
+  public int getPublicationDate() {
+    return publicationDate;
+  }
+
+  public void setPublicationDate(int publicationDate) {
+    this.publicationDate = publicationDate;
   }
 
   public static CarResponse from(Car car) {
