885dc1bdab9282ef16fc1e347c95b5f39c7c1213
sonanh2001 <anhnhs.b19cn026@stu.ptit.edu.vn>
2022-08-26 14:57:15 +0700

    feat : add constants for API URI and exception

---
+++ src/main/java/org/aibles/democar/constants/ApiConstants.java
@@ -0,0 +1,8 @@
+package org.aibles.democar.constants;
+
+public class ApiConstants {
+  public static final String VERSION = "v1";
+  public static final String CAR_RESOURCES = "cars";
+
+  public static final String CARS_API_URI = "api" + '/' + VERSION + '/' + CAR_RESOURCES;
+}
---
+++ src/main/java/org/aibles/democar/constants/ExceptionConstants.java
@@ -0,0 +1,7 @@
+package org.aibles.democar.constants;
+
+public class ExceptionConstants {
+  public static final Integer NOT_FOUND_STATUS = 404;
+  public static final String CAR_NOT_FOUND_CODE =
+      "org.aibles.democar.exception.CarNotFoundException";
+}
