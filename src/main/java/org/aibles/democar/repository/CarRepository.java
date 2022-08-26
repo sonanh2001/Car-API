92ca99419d37fc40e7b5a9ab09b5ba89a19070a5
sonanh2001 <anhnhs.b19cn026@stu.ptit.edu.vn>
2022-08-26 14:55:46 +0700

    build : add repository for Car object

---
+++ src/main/java/org/aibles/democar/repository/CarRepository.java
@@ -0,0 +1,10 @@
+package org.aibles.democar.repository;
+
+import org.aibles.democar.entity.Car;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CarRepository extends JpaRepository<Car, Long> {
+
+}
