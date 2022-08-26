71b05e8087e2b293fd5425a87aceb48fdc03b332
sonanh2001 <anhnhs.b19cn026@stu.ptit.edu.vn>
2022-08-26 14:53:34 +0700

    build : add dependencies, base packages, migration database

---
+++ src/test/java/org/aibles/democar/DemoCarApplicationTests.java
@@ -0,0 +1,13 @@
+package org.aibles.democar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
